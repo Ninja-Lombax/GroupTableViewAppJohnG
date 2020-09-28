e17c5ee81d1eb7a221ff583f31d29718499d9e09
Ninja-Lombax <godzillakotm80@gmail.com>
2020-09-28 12:14:59 -0500

    Initial Commit

---
+++ GroupTableViewAppJohnG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupTableViewAppJohnG
+//
+//  Created by John Grasser on 9/28/20.
+//  Copyright © 2020 John Grasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
